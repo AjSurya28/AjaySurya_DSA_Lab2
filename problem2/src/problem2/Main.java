8130e4622b3cbab2356fd3b6253f7408e4193130
AjSurya28 <119799232+AjSurya28@users.noreply.github.com>
2023-01-08 19:42:33 +0530

    Add files via upload

---
+++ problem2/src/problem2/Main.java
@@ -0,0 +1,31 @@
+package problem2;
+
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		MergeSort mergeSort = new MergeSort() ;
+		Count notesCount = new Count() ;
+		
+		System.out.println("enter the size of currency denominations ");
+		Scanner sc = new Scanner (System.in);
+		int size = sc.nextInt ();
+		int [] notes = new int[size];
+		System.out.println("enter the currency denominations value");
+		for (int i = 0; i < size; i++) {
+			notes[i] = sc.nextInt () ;
+		}
+		
+		System.out.println("enter the amount you want to pay");
+		int amount = sc.nextInt() ;
+		mergeSort.sort(notes, 0, notes.length-1);
+		notesCount.notesCountImplementation(notes,amount);
+		}
+
+		
+
+	}
+
+
